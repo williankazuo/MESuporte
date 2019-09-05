2b27cfd8d2a29c642b5ec79b03aa759bb8af7b5e
willian.kazuo@hotmail.com <willian.kazuo@hotmail.com>
2019-09-05 11:35:49 -0300

    docker file, envinroment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    urls: {
+      authenticationUrl: 'http://10.33.1.43:48500',
+      callSystemUrl: 'http://10.33.1.43:48501'
+    }
+  };
+  
